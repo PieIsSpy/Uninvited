a848bdbf712dfd06e31ee93827ed62182420ae1b
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-25 15:19:56 +0800

    feat: added constructors for entity and player

--- entities/entity.nim
+++ entities/entity.nim
@@ -1,5 +1,15 @@
 type Entity* = object of RootObj
+    name*: string
     health*: int
     max_health*: int
     damage*: int
     speed*: int
+
+proc create_entity*(name: string, health: int, damage: int, speed: int): Entity =
+    var new_entity: Entity
+    new_entity.name = name
+    new_entity.health = health
+    new_entity.max_health = health
+    new_entity.damage = damage
+    new_entity.speed = speed
+    return new_entity
--- entities/player.nim
+++ entities/player.nim
@@ -2,3 +2,13 @@ import entity
 
 type Player* = object of Entity
     perception*: int
+
+proc create_player*(): Player =
+    var new_player: Player
+    new_player.name = "Player"
+    new_player.health = 100
+    new_player.max_health = 100
+    new_player.damage = 5
+    new_player.speed = 1
+    new_player.perception = 0
+    return new_player
