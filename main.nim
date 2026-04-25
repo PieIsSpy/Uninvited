e47396836bdb71cd52cde12436c10cfef9cccddd
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-25 15:58:18 +0800

    feat: added showRules and main game loop call

--- main.nim
+++ main.nim
@@ -1,3 +1,12 @@
+import game
+
+proc showRules() =
+    echo("Run through the corridors as far as you can.")
+    echo("Get supplies along the way.")
+    echo("\x1B[31;1;4mDo NOT trust anyone.\x1B[0m")
+    echo("\x1B[31;1;4mDo NOT look back.\x1B[0m")
+    echo ""
+
 echo("Welcome, Uninvited Guest.")
 var name: string = ""
 while name == "" or name != "3":
@@ -7,8 +16,13 @@ while name == "" or name != "3":
     name = readLine(stdin)
 
     if name == "1":
-        echo 1
+        echo ""
+        echo("Enjoy your stay.")
+        echo ""
+        main()
     elif name == "2":
-        echo 2
+        echo ""
+        showRules()
     elif name == "3":
+        echo ""
         echo("Do not come back.")
