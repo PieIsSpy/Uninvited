a893936eec09258b618e32a010953499c89c10ec
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-25 18:07:21 +0800

    feat: changed player default damage to 10

--- entities/player.nim
+++ entities/player.nim
@@ -8,7 +8,7 @@ proc create_player*(): Player =
     new_player.name = "Player"
     new_player.health = 100
     new_player.max_health = 100
-    new_player.damage = 5
+    new_player.damage = 10
     new_player.speed = 1
     new_player.perception = 0
     return new_player
