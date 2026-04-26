fd5e18b7f6298c65eaedad0fc18207a2193add3f
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-26 16:56:32 +0800

    feat: removed relic on player

--- entities/player.nim
+++ entities/player.nim
@@ -1,11 +1,9 @@
 import entity
 import ../items/item
-import ../items/relic
 
 type Player* = object of Entity
     perception*: int
     items*: array[3, Item]
-    relic*: Relic
 
 proc create_player*(): Player =
     var new_player: Player
